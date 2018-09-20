dc6122c36870ec22baba627dfbc05b0d18adc0b2
BryantLee-atalent <bryant.lee@atalent.com>
2018-09-20 17:59:05 +0800

    Initial commit

---
+++ article/pipelines.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy.pipelines.images import ImagesPipeline
+from scrapy.exporters import JsonItemExporter
+import codecs, json, pymysql
+from twisted.enterprise import adbapi
+
+
+class ArticlePipeline(object):
+    def process_item(self, item, spider):
+        return item
+
+
+class JsonWithEncodingPipeline(object):
+    def __init__(self):
+        self.file = codecs.open("article.json", "w", encoding="utf-8")
+
+    def process_item(self, item, spider):
+        lines = json.dumps(dict(item), ensure_ascii=False) + "\n"
+        self.file.write(lines)
+        return item
+
+    def spider_closed(self, spider):
+        self.file.close()
+
+
+class JsonExporterPipline(object):
+    # 调用scrapy提供的JSON EXPORTER
+    def __init__(self):
+        self.file = open('articleexport.json', 'wb')
+        self.exporter = JsonItemExporter(self.file, encoding="utf-8", ensure_ascii=False)
+        self.exporter.start_exporting()
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
+
+    def process_item(self, item, spider):
+        self.exporter.export_item(item)
+        return item
+
+
+class MysqlTwistedPipline(object):
+    def __init__(self, dbpool):
+        self.dbpool = dbpool
+
+    @classmethod
+    def from_settings(cls, settings):
+        dbparms = dict(
+            host=settings["MYSQL_HOST"],
+            db=settings["MYSQL_DBNAME"],
+            user=settings["MYSQL_USER"],
+            passwd=settings["MYSQL_PASSWORD"],
+            charset='utf8',
+            use_unicode=True
+        )
+        dbpool = adbapi.ConnectionPool("pymysql", **dbparms)
+        return cls(dbpool)
+
+    def process_item(self, item, spider):
+        # 异步机制
+        query = self.dbpool.runInteraction(self.do_insert, item)
+        query.addErrback(self.handle_error)
+        return item
+
+    def handle_error(self, failure):
+        print(failure)
+
+    def do_insert(self, cursor, item):
+        insert_sql = '''
+            insert into job
+            values (%s, %s ,%s ,%s,%s,%s,%s,%s,%s,%s)
+        '''
+        cursor.execute(insert_sql, (
+        item["title"], item["create_date"], item["url"], item["url_object_id"], item["front_image_url"],
+        item["praise_nums"], item["comment_nums"], item["fav_nums"], item["tags"], item["content"]))
+
+
+class ArticleImagePipeline(ImagesPipeline):
+
+    def item_completed(self, results, item, info):
+        for ok, value in results:
+            image_file_path = value["path"]
+        item["front_image_path"] = image_file_path
+
+        return item
